8931d29a971562d5a6b28f91bcf6da0364f3f383
Y-oussef <yussuf.s.f@gmail.com>
2018-11-25 02:28:53 +0200

    Commit number test on GitHub #3

--- hellogit.c
+++ hellogit.c
@@ -5,6 +5,7 @@ void main()
   //Testing commit without add
   //now testing add, status then commit
   //Testing num of commits on Github, commit #1
-    //Testing num of commits on Github, commit #2
+  //Testing num of commits on Github, commit #2
+  //Testing num of commits on Github, commit #3
   printf("Hello tig");
 }
